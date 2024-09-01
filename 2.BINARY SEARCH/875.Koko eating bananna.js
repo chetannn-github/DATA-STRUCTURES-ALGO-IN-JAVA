769b2f18ea904eed57f60a93d010b68c0568106c
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-09-01 06:47:37 +0530

    Koko eating bananna

---
+++ 2.BINARY SEARCH/875.Koko eating bananna.js
@@ -0,0 +1,30 @@
+
+var minEatingSpeed = function(piles, h) {
+    let start = 1;
+    let end = Math.max(...piles);
+    let ans = end;
+    while(start<=end){
+        let mid = start + Math.floor((end-start)/2);
+       
+        if(speedPossible(mid,piles,h)){
+            ans = mid;
+            end = mid-1;
+
+        }else{
+            start = mid+1;
+        }
+   
+    }
+    return ans;
+   
+    };
+
+let speedPossible = (speed,piles,h)=>{
+    let ans = 0;
+    for (let i =0 ; i<piles.length; i++){
+        ans = ans + Math.ceil(piles[i]/speed);
+        if(ans>h){return false}
+        
+    }
+    return true;
+}
