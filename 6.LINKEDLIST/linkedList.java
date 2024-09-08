266c27afc057044d579eb77a14db9208a03fa1ac
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-09-08 16:50:27 +0530

    linked list implementation in java

--- 6.LINKEDLIST/linkedList.java
+++ 6.LINKEDLIST/linkedList.java
@@ -1,15 +1,73 @@
 
-class Main{
-    public static void main(String[] args) {
-        System.out.println("hiii");
-    }
-
-}
 
 class LinkedList {
-    public static class Node {
+    private static class Node {
         int data ;
-        int next;
+        Node next;
+
+    
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+
+        }
         
     }
+    private Node  head;
+
+    public LinkedList (){
+        head = null;
+    }
+
+    public void add(int data){
+        Node newNode = new Node(data);
+        if(head == null){
+            head = newNode;
+
+        }else{
+            Node current = head;
+            while(current.next != null){
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+    }
+
+    public void printList (){
+        Node current = head ;
+        while(current!= null){
+            System.out.println(current.data + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+
+    public int size (){
+        int count = 0;
+        Node current = head; 
+        while(current != null){
+
+            count++;
+            current = current.next;
+
+        }
+        return count;
+    }   
+
+    public boolean search (int data){
+        Node  current = head;
+        while(current != null){
+            if(current.data ==data){
+                return true;
+            }
+            current = current.next;
+        }
+        return false;
+    }
+
+   
+
+
+
 }
