13aa221f1a2ca1c1d03737241a88cd89f7d3eb10
Chetan Singh Rajawat <=chetan.rajawat25@gmail.com>
2024-09-08 16:28:32 +0530

    linked list implementation in java

---
+++ 6.LINKEDLIST/linkedList.java
@@ -0,0 +1,15 @@
+
+class Main{
+    public static void main(String[] args) {
+        System.out.println("hiii");
+    }
+
+}
+
+class LinkedList {
+    public static class Node {
+        int data ;
+        int next;
+        
+    }
+}
